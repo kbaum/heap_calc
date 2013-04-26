ef8ddab60dd910282774936ccd835c663ba47ee9
Karl Baum <karl.baum@gmail.com>
2013-04-26 17:24:33 -0400

    added minitest

---
+++ spec/heap_calc/calculation_spec.rb
@@ -0,0 +1,14 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+#require 'minitest_helper'
+
+describe 'HeapCalc::Calculation' do
+
+  it 'is pretty good' do
+    true.must_equal(true)
+  end
+
+
+end
