75b79d91e5dda4879d504870f6f18af3aca3bdb7
Karl Baum <karl.baum@gmail.com>
2013-04-29 08:17:15 -0400

    added implementation of object_breakdown

--- lib/heap_calc/calculation.rb
+++ lib/heap_calc/calculation.rb
@@ -1,5 +1,12 @@
 module HeapCalc
   class Calculation
+    def object_breakdown
+      GC.start
+      counts = Hash.new{ 0 }
+      ObjectSpace.each_object do |o|
+        counts[o.class] += 1
+      end
+      counts
+    end
   end
-
 end
--- spec/heap_calc/calculation_spec.rb
+++ spec/heap_calc/calculation_spec.rb
@@ -2,11 +2,33 @@ require 'minitest_helper'
 
 describe HeapCalc::Calculation do
 
-  before do
+  class HeapCalc::SomeObject
   end
 
-  it 'is pretty good' do
-    true.must_equal(true)
+  before do
+    @calculation = HeapCalc::Calculation.new
+  end
+
+  it 'breaks down the objects' do
+    @calculation.object_breakdown.wont_be_empty
+  end
+
+  describe '2 instances of SomeOjbect' do
+
+    it 'counts 2 instances of HeapCalc::SomeObject' do
+      objects = 2.times.collect{ HeapCalc::SomeObject.new }
+      @calculation.object_breakdown[HeapCalc::SomeObject].must_equal 2
+    end
+
+  end
+
+  describe '3 instances of SomeOjbect' do
+
+    it 'counts 2 instances of HeapCalc::SomeObject' do
+      objects = 3.times.collect{ HeapCalc::SomeObject.new }
+      @calculation.object_breakdown[HeapCalc::SomeObject].must_equal 3
+    end
+
   end
 
 
